8b579dd77be8f887895576905d77467665fbf6b7
Flojito <flojito@archireport.com>
2025-02-26 03:41:38 +0100

    fix: button should call onMenuChange

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
+import { Page, useMenuContext } from '@/app/contexts/menuProvider';
 import { generateUniqueId } from '@/app/utils/id';
 import { useLocalStorage } from '@/app/utils/localStorage';
-import Link from 'next/link';
 import { useEffect } from 'react';
 
 export default function Home() {
+  const { onMenuChange } = useMenuContext();
   const [id, setId] = useLocalStorage('user_id', '');
 
   useEffect(() => {
@@ -17,12 +18,12 @@ export default function Home() {
 
   return (
     <div className="min-h-[calc(100dvh-4rem)] flex items-center justify-center bg-background">
-      <Link
-        href="/homes"
+      <button
         className="px-8 py-4 text-2xl font-medium bg-primary text-white rounded-lg hover:bg-primary/90 transition-colors"
+        onClick={() => onMenuChange(Page.Homes)}
       >
         Ajouter un bien
-      </Link>
+      </button>
     </div>
   );
 }
