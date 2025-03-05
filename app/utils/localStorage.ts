6f1686eb8044b3b0a419b12f840f9acdc102908b
Flojito <flojito@archireport.com>
2025-03-05 02:24:15 +0100

    refactor: add get & set

--- app/utils/localStorage.ts
+++ app/utils/localStorage.ts
@@ -17,27 +17,48 @@ export function useLocalStorage<T>(key: string, defaultState: T): [T, Dispatch<S
 
   const isFirstRenderRef = useRef(true);
   useEffect(() => {
-    if (!key) {
-      console.warn('useLocalStorage: key is not defined');
-      return;
-    }
-
     if (isFirstRenderRef.current) {
       isFirstRenderRef.current = false;
       return;
     }
-    try {
-      if (value === undefined) {
-        localStorage.removeItem(key);
-      } else {
-        localStorage.setItem(key, JSON.stringify(value));
-      }
-    } catch (error) {
-      if (typeof window !== 'undefined') {
-        console.error(error);
-      }
-    }
+    setLocalStorageItem(key, value);
   }, [value, key]);
 
   return state;
+}
+
+export function getLocalStorageItem<T>(key: string, defaultValue: T) {
+  if (!key) {
+    console.warn('useLocalStorage: key is not defined');
+    return defaultValue;
+  }
+
+  try {
+    const value = localStorage.getItem(key);
+    if (value) return JSON.parse(value) as T;
+  } catch (error) {
+    if (typeof window !== 'undefined') {
+      console.error(error);
+    }
+  }
+  return defaultValue;
+}
+
+export function setLocalStorageItem<T>(key: string, value: T) {
+  if (!key) {
+    console.warn('useLocalStorage: key is not defined');
+    return;
+  }
+
+  try {
+    if (value === undefined) {
+      localStorage.removeItem(key);
+    } else {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
+  } catch (error) {
+    if (typeof window !== 'undefined') {
+      console.error(error);
+    }
+  }
 }
