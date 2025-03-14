6a6846c3903b281fe6f4c1eabb463ad9c99db206
Flojito <flojito@archireport.com>
2025-03-14 23:20:42 +0100

    style: adjust floating button

--- app/components/floatingActionButton.tsx
+++ app/components/floatingActionButton.tsx
@@ -19,7 +19,7 @@ export default function FloatingActionButton({
     <button
       onClick={onClick}
       className={clsx(
-        'fixed bottom-[108px] right-6 w-14 h-14 rounded-full bg-primary text-background flex items-center justify-center shadow-lg',
+        'fixed bottom-24 right-4 w-14 h-14 rounded-full bg-primary text-background flex items-center justify-center shadow-lg',
         'hover:bg-primary/90 transition-colors z-10',
         className,
       )}
