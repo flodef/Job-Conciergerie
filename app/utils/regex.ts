b9d71c7b015027e7973b81826bcf524226096d56
Flojito <flojito@archireport.com>
2025-04-10 17:11:14 +0200

    fix: textarea accepts carriage return

--- app/utils/regex.ts
+++ app/utils/regex.ts
@@ -1,5 +1,5 @@
 export const inputLengthRegex = /^.{0,32}$/;
-export const messageLengthRegex = /^.{0,500}$/;
+export const messageLengthRegex = /^[\s\S]{0,500}$/;
 export const emailRegex = /^[a-zA-Z0-9._%+-]{1,64}@[a-zA-Z0-9.-]{1,64}\.[a-zA-Z]{2,22}$/;
 export const frenchPhoneRegex = /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/;
 
